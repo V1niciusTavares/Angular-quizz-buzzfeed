f06a488e8c92af1fa4beecea06207114096abf26
v1niciustavares <viniciusmtz13@gmail.com>
2024-02-26 19:32:32 -0300

    Projeto criado

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/buzzfeed'),
+      dir: require('path').join(__dirname, './coverage/projeto-buzzfeed'),
       subdir: '.',
       reporters: [
         { type: 'html' },
